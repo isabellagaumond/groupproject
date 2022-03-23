8b1b4370b4f49e3e5dfd1597a4efad45e7522ff3
isabellagaumond <101876586+isabellagaumond@users.noreply.github.com>
2022-03-23 11:21:12 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -194,6 +194,7 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Goal, function (sprite, othe
     basketScoreZone.setPosition(basket.x, basket.y - 10)
     ballSpawn()
     scoreboardSpriteChange()
+    appear(star)
 })
 function scoreboardSpriteChange () {
     if (scoreValue == 1) {
@@ -898,10 +899,33 @@ function scoreboardSpriteChange () {
             `)
     }
 }
+function appear (mySprite: Sprite) {
+    star = sprites.create(img`
+        . . . . . . . b b . . . . . . . 
+        . . . . . . b d d b . . . . . . 
+        . . . . . b d 5 5 d b . . . . . 
+        . . . . b b 5 5 5 5 b b . . . . 
+        . . . . b 5 5 5 5 5 5 b . . . . 
+        b b b b b 5 5 5 5 1 1 d b b b b 
+        b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b 
+        b d d 5 5 5 5 5 5 1 1 1 5 d d b 
+        . b d d 5 5 5 5 5 5 5 5 d d b . 
+        . . b b 5 5 5 5 5 5 5 5 b b . . 
+        . . c b 5 5 5 5 5 5 5 5 b c . . 
+        . . c 5 5 5 5 d d 5 5 5 5 c . . 
+        . . c 5 5 d b b b b d 5 5 c . . 
+        . . c 5 d b c c c c b d 5 c . . 
+        . . c c c c . . . . c c c c . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    star.setPosition(75, 60)
+    star.destroy(effects.starField, 500)
+}
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.thing, function (sprite, otherSprite) {
     myDart.sprite.destroy()
     ballSpawn()
 })
+let star: Sprite = null
 let myDart: Dart = null
 let scoreValue = 0
 let basketScoreZone: Sprite = null
